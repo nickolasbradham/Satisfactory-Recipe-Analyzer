532c0432bfb99f6e205bd3bbd9c0931eacb0b248
nickolasbradham <nickolasbradham@gmail.com>
2025-04-26 17:03:15 -0600

    Fixed Excited Photonic Matter.

--- src/nbradham/satisAnalize/Analyzer.java
+++ src/nbradham/satisAnalize/Analyzer.java
@@ -10,9 +10,25 @@ import java.util.Scanner;
 final class Analyzer {
 
 	private final HashMap<String, Item> items = new HashMap<>();
+	private final HashSet<Item> queSet = new HashSet<>();
+	Node head = null, last = null;
 
 	private final void start() throws FileNotFoundException {
-		Scanner scan = createScanner("recipes.tsv");
+		Scanner scan = createScanner("rates.tsv");
+		int max = 0;
+		while (scan.hasNext()) {
+			final Item item = getOrCreateItem(scan.next());
+			final int tMax = item.weight = scan.nextInt();
+			if (tMax > max)
+				max = tMax;
+			enqueue(item);
+		}
+		Node n = head;
+		while (n != null) {
+			n.item.weight = max / n.item.weight;
+			n = n.next;
+		}
+		scan = createScanner("recipes.tsv");
 		while (scan.hasNext()) {
 			Recipe r = new Recipe(scan.next(), parseItems(scan.next()), scan.next(), parseItems(scan.next()));
 			r.inputs.keySet().forEach(i -> {
@@ -21,46 +37,40 @@ final class Analyzer {
 				recs[l] = r;
 				i.consumers = recs;
 			});
-		}
-		scan.close();
-		scan = createScanner("rates.tsv");
-		int max = 0;
-		Node head = null, last = null;
-		final HashSet<Item> queSet = new HashSet<>();
-		while (scan.hasNext()) {
-			final Item item = items.get(scan.next());
-			final int tMax = item.weight = scan.nextInt();
-			if (tMax > max)
-				max = tMax;
-			if (!queSet.contains(item)) {
-				queSet.add(item);
-				final Node n = new Node(item);
-				if (head == null)
-					last = head = n;
-				else
-					last = last.next = n;
-			}
-		}
-		Node n = head;
-		while (n != null) {
-			n.item.weight = max / n.item.weight;
-			n = n.next;
+			if (r.inputs.size() == 0)
+				r.outputs.keySet().forEach(i -> {
+					i.weight = 0;
+					enqueue(i);
+				});
 		}
 	}
 
 	private final HashMap<Item, Float> parseItems(final String parse) {
 		final HashMap<Item, Float> map = new HashMap<>();
-		for (final String split : parse.split(", ")) {
-			final int x = split.indexOf('x');
-			if (x != -1) {
-				final String name = split.substring(x + 1);
-				Item i = items.get(name);
-				if (i == null)
-					items.put(name, i = new Item());
-				map.put(i, Float.parseFloat(split.substring(0, x)));
+		if (!parse.isBlank())
+			for (final String split : parse.split(", ")) {
+				final int x = split.indexOf('x');
+				map.put(getOrCreateItem(split.substring(x + 1)), Float.parseFloat(split.substring(0, x)));
 			}
-		}
 		return map;
+	}
+
+	private final Item getOrCreateItem(String name) {
+		Item i = items.get(name);
+		if (i == null)
+			items.put(name, i = new Item());
+		return i;
+	}
+
+	private final void enqueue(Item item) {
+		if (!queSet.contains(item)) {
+			queSet.add(item);
+			final Node n = new Node(item);
+			if (head == null)
+				last = head = n;
+			else
+				last = last.next = n;
+		}
 	}
 
 	private static final Scanner createScanner(final String filename) throws FileNotFoundException {
