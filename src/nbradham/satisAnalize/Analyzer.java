95465818f9ad021e0251eacf24dca48d59052ab4
nickolasbradham <nickolasbradham@gmail.com>
2025-04-26 16:39:18 -0600

    Foundation rebuild.

--- src/nbradham/satisAnalize/Analyzer.java
+++ src/nbradham/satisAnalize/Analyzer.java
@@ -4,131 +4,88 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map.Entry;
-import java.util.Queue;
+import java.util.HashSet;
 import java.util.Scanner;
 
 final class Analyzer {
 
-	private static final HashMap<String, Item> ITEMS = new HashMap<>();
+	private final HashMap<String, Item> items = new HashMap<>();
 
 	private final void start() throws FileNotFoundException {
-		Scanner scan = createScanner("rates.tsv");
-		int max = Integer.MIN_VALUE;
-		Queue<Item> updateQueue = new LinkedList<>();
-		while (scan.hasNext()) {
-			String name = scan.next();
-			int weight = scan.nextInt();
-			Item i = new Item(name, weight);
-			ITEMS.put(name, i);
-			if (weight > max)
-				max = weight;
-			updateQueue.offer(i);
-		}
-		scan.close();
-		for (Item i : ITEMS.values())
-			i.weight = max / i.weight;
-		System.out.printf("Raw Resources: %s%n", ITEMS);
-		scan = createScanner("recipes.tsv");
+		Scanner scan = createScanner("recipes.tsv");
 		while (scan.hasNext()) {
 			Recipe r = new Recipe(scan.next(), parseItems(scan.next()), scan.next(), parseItems(scan.next()));
-			if (r.input.size() == 0)
-				r.output.keySet().forEach(k -> {
-					k.weight = 0;
-					k.best = r;
-				});
-			else
-				r.input.keySet().forEach(k -> {
-					int l = k.consumers.length;
-					k.consumers = Arrays.copyOf(k.consumers, l + 1);
-					k.consumers[l] = r;
-				});
-			r.output.keySet().forEach(i -> {
-				if (i.weight == Float.MAX_VALUE)
-					i.best = r;
+			r.inputs.keySet().forEach(i -> {
+				final int l = i.consumers.length;
+				Recipe[] recs = Arrays.copyOf(i.consumers, l + 1);
+				recs[l] = r;
+				i.consumers = recs;
 			});
 		}
 		scan.close();
-		System.out.println("Items:");
-		ITEMS.forEach((k, v) -> System.out.printf("\t%s: %s%n", k, v));
-		while (updateQueue.size() != 0) {
-			consumers: for (Recipe rec : updateQueue.poll().consumers) {
-				System.out.printf("Checking %s...%n", rec.name);
-				float sum = 0;
-				for (Entry<Item, Float> e : rec.input.entrySet()) {
-					float w = e.getKey().weight;
-					if (w == Float.MAX_VALUE)
-						continue consumers;
-					sum += w * e.getValue();
-				}
-				System.out.println("\tUpdating outputs...");
-				for (Entry<Item, Float> e : rec.output.entrySet()) {
-					float w = sum / e.getValue();
-					Item i = e.getKey();
-					if (i.weight > w) {
-						System.out.printf("\t\t%s: %s, %.3f -> %.3f%n", i.name, i.best.name, i.weight, i.weight = w);
-						i.best = rec;
-						updateQueue.offer(i);
-					}
-				}
+		scan = createScanner("rates.tsv");
+		int max = 0;
+		Node head = null, last = null;
+		final HashSet<Item> queSet = new HashSet<>();
+		while (scan.hasNext()) {
+			final Item item = items.get(scan.next());
+			final int tMax = item.weight = scan.nextInt();
+			if (tMax > max)
+				max = tMax;
+			if (!queSet.contains(item)) {
+				queSet.add(item);
+				final Node n = new Node(item);
+				if (head == null)
+					last = head = n;
+				else
+					last = last.next = n;
 			}
 		}
-		System.out.println("Best Recipes:");
-		ITEMS.forEach((k, v) -> System.out.printf("%s\t%s%n", k, v.best.name));
+		Node n = head;
+		while (n != null) {
+			n.item.weight = max / n.item.weight;
+			n = n.next;
+		}
 	}
 
-	private final HashMap<Item, Float> parseItems(String str) {
-		HashMap<Item, Float> items = new HashMap<>();
-		if (str.length() != 0)
-			for (String s : str.split(", ")) {
-				int x = s.indexOf('x');
-				String name = s.substring(x + 1);
-				Item i = ITEMS.get(name);
+	private final HashMap<Item, Float> parseItems(final String parse) {
+		final HashMap<Item, Float> map = new HashMap<>();
+		for (final String split : parse.split(", ")) {
+			final int x = split.indexOf('x');
+			if (x != -1) {
+				final String name = split.substring(x + 1);
+				Item i = items.get(name);
 				if (i == null)
-					ITEMS.put(name, i = new Item(name));
-				items.put(i, Float.parseFloat(s.substring(0, x)));
+					items.put(name, i = new Item());
+				map.put(i, Float.parseFloat(split.substring(0, x)));
 			}
-		return items;
+		}
+		return map;
 	}
 
-	private static Scanner createScanner(String filename) throws FileNotFoundException {
+	private static final Scanner createScanner(final String filename) throws FileNotFoundException {
 		return new Scanner(new File(filename)).useDelimiter("\t|\r*\n").skip(".*\r*\n");
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static final void main(final String[] args) throws FileNotFoundException {
 		new Analyzer().start();
 	}
 
-	private static final class Item {
+	private static final record Recipe(String name, HashMap<Item, Float> inputs, String machine,
+			HashMap<Item, Float> outputs) {
+	}
 
-		private static final Recipe NULL_RECIPE = new Recipe(null, null, null, null);
+	private static final class Node {
+		private final Item item;
+		private Node next;
 
-		private final String name;
-
-		private Recipe[] consumers = new Recipe[0];
-		private Recipe best = NULL_RECIPE;
-		private float weight;
-
-		public Item(String setName) {
-			this(setName, Float.MAX_VALUE);
-		}
-
-		private Item(String setName, float initialWeight) {
-			name = setName;
-			weight = initialWeight;
-		}
-
-		@Override
-		public final String toString() {
-			String[] recipes = new String[consumers.length];
-			for (byte i = 0; i != consumers.length; ++i)
-				recipes[i] = consumers[i].name;
-			return String.format("(W: %.3f, B: %s, C: %s)", weight, best.name, Arrays.toString(recipes));
+		private Node(Item setItem) {
+			item = setItem;
 		}
 	}
 
-	private static final record Recipe(String name, HashMap<Item, Float> input, String machine,
-			HashMap<Item, Float> output) {
+	private static final class Item {
+		private Recipe[] consumers = new Recipe[0];
+		private int weight = -1;
 	}
 }
